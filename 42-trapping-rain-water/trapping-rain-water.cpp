7a513c307ca96b202ed1ce8f76d1f3aa453562f0
Parashonashetti <honashettiparas@gmail.com>
2026-08-27 19:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (77.68%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,25 @@
+//leetcode extention 
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int count = 0;
+        int l = 0;
+        int r = n-1;
+        int leftmax =0;
+        int rightmax = 0;
+        while(l<r){
+            leftmax = max(leftmax, height[l]);
+            rightmax = max(rightmax, height[r]);
+            if (leftmax < rightmax ){
+                count+= leftmax - height[l];
+                l++; 
+
+            }else{
+                count+= rightmax - height[r];
+                r--;
+            }
+        }
+        return count;
+    }
+};
