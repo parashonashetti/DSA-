0da4a46d2a8c520a4ad785d156c3b8d5b1ebbeac
Parashonashetti <honashettiparas@gmail.com>
2026-08-31 18:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (95.20%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
         int freq = 0;
         int ans = 0;
-        for(int i =0; i<n; i++){
-            if (freq == 0 ){
+        for(int i = 0; i < n; i++){
+            if(freq == 0){
                 ans = nums[i];
             }
-             if(ans == nums[i]){
+            if(ans == nums[i]){
                 freq++;
             }else{
-                freq--;
+                 freq--;
             }
-        }
+         
+         }       
         return ans;
-        
     }
 };
